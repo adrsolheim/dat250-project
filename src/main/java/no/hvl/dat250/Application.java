da13e84471620b7fab0e9c69bbecfb59407ccd87
adrsolheim <adrsolheim2@gmail.com>
2021-09-14 14:53:35 +0200

    Test methods for querying database

--- src/main/java/no/hvl/dat250/Application.java
+++ src/main/java/no/hvl/dat250/Application.java
@@ -1,7 +1,40 @@
 package no.hvl.dat250;
 
+import no.hvl.dat250.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
 public class Application {
+    private static final String PERSISTENCE_UNIT_NAME = "feedapp";
+    private static EntityManagerFactory factory;
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+        fetchUsers(em);
+
+    }
+
+    public static void fetchUsers(EntityManager em) {
+        Query q = em.createQuery("select u from User u");
+        List<User> userList = q.getResultList();
+        if (userList.size() == 0) {
+            System.out.println("Creating user..");
+            em.getTransaction().begin();
+            User user = new User("Bob", "bob@mail.com", "badpassword", false);
+            em.persist(user);
+            em.getTransaction().commit();
+            q = em.createQuery("select u from User u");
+            userList = q.getResultList();
+        }
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
     }
 }
