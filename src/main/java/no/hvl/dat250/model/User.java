de75b0440d38664b79f2dc71ce00027cbbf7b05a
adrsolheim <adrsolheim2@gmail.com>
2021-09-14 14:45:46 +0200

    Implement user entity

---
+++ src/main/java/no/hvl/dat250/model/User.java
@@ -0,0 +1,71 @@
+package no.hvl.dat250.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String username;
+    String email;
+    String password;
+    boolean admin;
+
+    public User() {
+
+    }
+    public User(String username, String email, String password, boolean admin) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.admin = admin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+    @Override
+    public String toString() {
+        return username;
+    }
+}
