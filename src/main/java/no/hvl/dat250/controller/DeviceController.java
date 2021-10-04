24ed6c42256b76d502966a9abb7a4e6051496856
adrsolheim <adrsolheim2@gmail.com>
2021-10-04 22:42:00 +0200

    Add REST endpoints for CRUD operations

---
+++ src/main/java/no/hvl/dat250/controller/DeviceController.java
@@ -0,0 +1,11 @@
+package no.hvl.dat250.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeviceController {
+
+}
---
+++ src/main/java/no/hvl/dat250/controller/PollController.java
@@ -0,0 +1,82 @@
+package no.hvl.dat250.controller;
+
+import no.hvl.dat250.dao.PollDAO;
+import no.hvl.dat250.exceptions.PollNotFoundException;
+import no.hvl.dat250.model.Poll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PollController {
+
+    @Autowired
+    private PollDAO pollDAO;
+
+    @GetMapping("/polls")
+    public List<Poll> polls() {
+        return pollDAO.getAll();
+    }
+
+    @GetMapping("/polls/{id}")
+    public ResponseEntity<Object> poll(@PathVariable long id) {
+        Optional<Poll> poll = pollDAO.get(id);
+        //return poll.orElseThrow(() -> new PollNotFoundException(
+         //       String.format("User not found, id: %d",id)
+        //));
+        if(!poll.isPresent()) {
+            return new ResponseEntity<>("Poll not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(poll, HttpStatus.FOUND);
+    }
+
+    @RequestMapping(value = "/poll", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> createPoll(@RequestBody Poll poll) {
+        pollDAO.save(poll);
+        return new ResponseEntity<>("Poll is created successfully!", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/polls/{id}")
+    public ResponseEntity<Object> updatePoll(@PathVariable("id") long id, @RequestBody Poll poll) {
+        Optional<Poll> dbPoll = pollDAO.get(id);
+        if(!dbPoll.isPresent()) {
+            return new ResponseEntity<>("Could not update. Poll does not exist", HttpStatus.NOT_FOUND);
+        }
+        Object[] params = {poll.getQuestion(), poll.getYesVote(), poll.getNoVote(),
+                poll.getIsPublic(), poll.getCode(), poll.getDuration(), poll.getUserAccount()};
+        pollDAO.update(dbPoll.get(), params);
+        return new ResponseEntity<>("Poll successfully updated!", HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/polls/{id}/yes")
+    public ResponseEntity<Object> voteYes(@PathVariable("id") long id) {
+        Optional<Poll> dbPoll = pollDAO.get(id);
+        if(!dbPoll.isPresent()) {
+            return new ResponseEntity<>("Could not update. Poll does not exist", HttpStatus.NOT_FOUND);
+        }
+        Poll poll = dbPoll.get();
+        Object[] params = {poll.getQuestion(), poll.getYesVote() + 1, poll.getNoVote(),
+                poll.getIsPublic(), poll.getCode(), poll.getDuration(), poll.getUserAccount()};
+        pollDAO.update(dbPoll.get(), params);
+        return new ResponseEntity<>("You voted 'Yes'", HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/polls/{id}/no")
+    public ResponseEntity<Object> voteNo(@PathVariable("id") long id) {
+        Optional<Poll> dbPoll = pollDAO.get(id);
+        if(!dbPoll.isPresent()) {
+            return new ResponseEntity<>("Couldn't find poll.", HttpStatus.NOT_FOUND);
+        }
+        Poll poll = dbPoll.get();
+        Object[] params = {poll.getQuestion(), poll.getYesVote(), poll.getNoVote() + 1,
+                poll.getIsPublic(), poll.getCode(), poll.getDuration(), poll.getUserAccount()};
+        pollDAO.update(dbPoll.get(), params);
+        return new ResponseEntity<>("You voted 'No'", HttpStatus.ACCEPTED);
+    }
+}
+
