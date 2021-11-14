4f4417b75e003527d8cac2531004bcec6cc1e164
adrsolheim <adrsolheim2@gmail.com>
2021-11-14 14:36:31 +0100

    / now points to homepage

--- src/main/java/no/hvl/dat250/controller/HomeController.java
+++ src/main/java/no/hvl/dat250/controller/HomeController.java
@@ -1,19 +1,12 @@
 package no.hvl.dat250.controller;
 
-import no.hvl.dat250.model.Poll;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    @ResponseBody
     public String hello() {
-        return "Welcome to /";
+        return "index.html";
     }
 }
